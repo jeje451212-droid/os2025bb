93f6cdb74939971412d2f3f946905f6843b2a1a0
이예지 <jeje451212@gmail.com>
2025-09-29 15:32:46 +0900

    .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -7,15 +7,7 @@ import (
 
 func main() {
 	//Zero values
-	var f64 float64
-	var i16 int16
-	var t bool
-	var s string
-	var i int
+	totalPrice := 1000
 
-	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(i16, reflect.TypeOf(i16))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
+	fmt.Println(totalPrice, reflect.TypeOf(totalPrice))
 }
